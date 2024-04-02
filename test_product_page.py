6cc42ae7a3fa0cc6ee23193709b538fb03663196
Inna <idinna91@mail.ru>
2024-04-02 21:13:12 +0300

    refactor

--- test_product_page.py
+++ test_product_page.py
@@ -12,20 +12,20 @@ class TestUserAddToBasketFromProductPage:
     def setup(self, browser):
         f = faker.Faker()
         email = f.email()
-        link = 'http://selenium1py.pythonanywhere.com/'
+        link = "http://selenium1py.pythonanywhere.com/"
         signup = LoginPage(browser, link)
         signup.open()
         signup.register_new_user(email=email, password="12323123456")
         signup.should_be_authorized_user()
 
     def test_user_cant_see_success_message(self, browser):
-        link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
         page = ProductPage(browser, link)
         page.open()
         page.should_not_be_success_message()
 
     def test_user_can_add_product_to_basket(self, browser):
-        link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
         page = ProductPage(browser, link)
         page.open()
         page.add_to_basket()
@@ -46,7 +46,7 @@ class TestUserAddToBasketFromProductPage:
 #                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
 #                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser):
-    link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     # link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
     page = ProductPage(browser, link)
     page.open()
@@ -56,7 +56,7 @@ def test_guest_can_add_product_to_basket(browser):
 
 
 def test_guest_cant_see_success_message(browser):
-    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
